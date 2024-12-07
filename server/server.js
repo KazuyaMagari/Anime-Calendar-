53c38a8656c45e73b32d01ff42d3a4a04b37e879
KazuyaMagari <kazuyamagarifuchi1211@gmail.com>
2024-12-06 20:46:59 -0500

    update cors

--- server/server.js
+++ server/server.js
@@ -1,2 +1,6 @@
 const express = require('express');
-const App = express();
+const app = express();
+const cors = require('cors');
+const corsOptions = {origin: ["http//localhost:5173"]}
+
+app.use(cors(corsOptions));

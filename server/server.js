e3c946e303951eb5f43e146b235adf813808702b
KazuyaMagari <kazuyamagarifuchi1211@gmail.com>
2024-12-06 21:16:15 -0500

    connect express and react

--- server/server.js
+++ server/server.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const corsOptions = {origin: ["http//localhost:5173"]}
+const corsOptions = {origin: ["http://localhost:5173"]}
 
 app.use(cors(corsOptions));
 
+app.get('/', (req, res)=> {
+    
+    
+})
+
+app.listen(3001, ()=> {
+    console.log('hello')
+})
